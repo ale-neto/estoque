af3c2a4f7e858377f39bede0bf1c66e72cd30061
ale-neto <alexandreneto.dev@gmail.com>
2020-04-30 19:00:18 -0300

    Update
    Inserido a opção de update

--- src/main/java/com/estoque/controller/EstoqueController.java
+++ src/main/java/com/estoque/controller/EstoqueController.java
@@ -4,8 +4,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -45,15 +47,31 @@ public class EstoqueController {
 	}
 
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ModelAndView detalheNota(@PathVariable("id") long id){
+	@RequestMapping(value="/detalhe/{id}", method=RequestMethod.GET)
+	public String detalheNota(@PathVariable("id") long id, Model model){
 		EstoqueModel estoque = estoqueRepository.findById(id);
-		ModelAndView mv = new ModelAndView("estoque/detalheNota");
-		mv.addObject("estoque", estoque);
+		model.addAttribute("estoque", estoque);
 		Iterable<ProdutoModel> produto = produtoRepository.findByEstoque(estoque);
-		mv.addObject("produtos", produto);
-		return mv;
+		model.addAttribute("produtos", produto);
+		return "estoque/detalheNota";
 	}
+	
+	@RequestMapping(value="/atualizar/{id}", method=RequestMethod.GET)
+	public String atualizarNota(@PathVariable("id") long id, Model model){
+		EstoqueModel estoque = estoqueRepository.findById(id);
+		model.addAttribute("estoque", estoque);
+		Iterable<ProdutoModel> produto = produtoRepository.findByEstoque(estoque);
+		model.addAttribute("produtos", produto);
+		return "estoque/atualizarEstoque";
+	}
+	
+	@RequestMapping(value="/update/{id}", method=RequestMethod.POST)
+	public String updateEstoque(@PathVariable("id") long id, @Valid EstoqueModel estoque, BindingResult result, Model model){
+		estoque.setId(id);
+		estoqueRepository.save(estoque);
+		return "redirect:/";	
+	}
+	
 	
 	@RequestMapping(value="/deletarEstoque")
 	public String deletarEstoque(long id){
@@ -62,17 +80,17 @@ public class EstoqueController {
 		return "redirect:/estoque";
 	}
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.POST)
+	@RequestMapping(value="/detalhe/{id}", method=RequestMethod.POST)
 	public String detalheNotaP(@PathVariable("id") long id, @Valid ProdutoModel produto, BindingResult result, RedirectAttributes attributes){
 		if(result.hasErrors()){
 			attributes.addFlashAttribute("mensagem", "Verifique os campos!");
-			return "redirect:/{id}";
+			return "redirect:/detalhe/{id}";
 		}
 		EstoqueModel estoque = estoqueRepository.findById(id);
 		produto.setEstoque(estoque);
 		produtoRepository.save(produto);
 		attributes.addFlashAttribute("mensagem", "Produto adicionado com sucesso!");
-		return "redirect:/{id}";
+		return "redirect:/detalhe/{id}";
 	}
 	
 	@RequestMapping(value="/deletarProduto")
